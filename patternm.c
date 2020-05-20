bdccb19af999d5ab8749e49736e8230625663080
Luigi Foscari <luigi.foscari@icloud.com>
2020-05-20 11:53:28 +0200

    now it works

--- patternm.c
+++ patternm.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
 
 char *read_text(int *dim)
@@ -9,7 +8,7 @@ char *read_text(int *dim)
 	char *s = malloc(size * sizeof(char)), t;
 
 	while(( t = getchar() ) != '\n') {
-		if(t > 127 || t < 33)
+		if(t > 126 || t < 33)
 			continue;
 
 		if(++i >= size) {
@@ -18,6 +17,7 @@ char *read_text(int *dim)
 		}
 		*(s + i - 1) = t;
 	}
+
 	s = realloc(s, i * sizeof(char) + 1);
 	*(s + i + 1) = '\0';
 	*dim = i;
@@ -26,9 +26,9 @@ char *read_text(int *dim)
 
 int main(int argvc, char **argv)
 {
-	int q, d = 256, p = 0, t = 0, match = 0, n, m;
+	int q, h = 1, d = 256, p = 0, t = 0, n, m;
 	char *text, *pattern;
-	
+
 	printf("Rabin-Karp algorithm - Pattern matching\n");
 
 	printf("Text: ");
@@ -39,9 +39,12 @@ int main(int argvc, char **argv)
 
 	printf("Modulo [%d]: ", m);
 	scanf(" %d", &q);
-	
-	long h = (int) pow(d, m - 1) % q;
 
+    // d^(m - 1)
+    for(int i = 0; i < m - 1; i++)
+        h = ( h * d ) % q;
+
+    // Compute initial hash
 	for(int i = 0; i < m; i++) {
 		p = ( d * p + pattern[i] ) % q;
 		t = ( d * t + text[i] ) % q;
@@ -49,20 +52,17 @@ int main(int argvc, char **argv)
 
 	for(int s = 0; s <= n - m; s++) {
 		if(p == t) {
-			char temp[m];
-			strncpy(temp, text + s, m);
-			if(strcmp(temp, pattern) == 0) {
-				printf("|> Match\tshift: %d\n", s);
-				match++;
-			}
-		}
-		if(s < n - m)
-			t = ( d * ( t - h * text[s] ) + text[s + m] ) % q;
-		if(t < 0)
-			t += q;
+            int i;
+            for(i = 0; i < m && text[s + i] != pattern[i]; i++);
+            if(i >= m)
+                printf("Match at offset %d\n", s);
+        }
+		if(s < n - m) {
+			t = ( ( t - h * text[s] ) * d + text[s + m] ) % q;
+		    if(t < 0)
+			    t += q;
+        }
 	}
-
-	printf("Number of matches: %d\n", match);
 
 	return 0;
 }
